6238ac5d2cd4cceb237e6801fdaef41e74b6877d
leoci <leonardo@localhost.localdomain>
2022-03-24 13:14:27 -0400

    Just code

--- src/Components/Mini/Image.jsx
+++ src/Components/Mini/Image.jsx
@@ -7,10 +7,10 @@ export function Image ({ src, alt, special = null }) {
             <img className='object-contain h-fit' src={src} alt={alt} />
             </picture>
           : <picture className='flex rounded-lg overflow-x-auto'>
-            <img className='object-contain pr-12 h-fit' src='1.webp' alt={alt} />
+            <img className='object-contain pr-12 h-fit' src='4.webp' alt={alt} />
             <img className='object-contain pr-12 h-fit' src='2.webp' alt={alt} />
             <img className='object-contain pr-12 h-fit' src='3.webp' alt={alt} />
-            <img className='object-contain h-fit' src='4.webp' alt={alt} />
+            <img className='object-contain h-fit' src='1.webp' alt={alt} />
           </picture>
   }
     </div>
