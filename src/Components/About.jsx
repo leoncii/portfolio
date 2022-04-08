1fa9f621cc92417a112183c8df1ff3d1d9c86dd2
leoncii <leonardotobe94@gmail.com>
2022-04-08 00:21:48 -0400

    Add console nmap typewriting :D

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ export function About () {
         Soy...
       </Title5>
       <Description only>
-        Un desarrollador de aplicaciones web y escritorio de confianza, estoy de freelancer trabajando con Next.js y el ecosistema <strong>JavaScript</strong>, como hobby me gusta 'buscar la bandera' en Hack The Box.
+        Un desarrollador de aplicaciones web y escritorio de confianza, en este momento estoy de freelancer trabajando con Next.js y el ecosistema <strong>JavaScript</strong> en general, como hobby o momentos libres me gusta 'buscar la bandera' en Hack The Box.
       </Description>
     </>
   )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,19 @@
 export function Hero () {
   return (
-    <section className='bg-[#101010] w-full h-screen pl-9 pr-9 mb-32 md:mb-0'>
+    <section className='bg-[#101010] w-full h-screen pl-4 md:pl-9 pr-4 md:pr-9 mb-32 md:mb-0'>
       <div className='flex justify-between max-w-[1200px] mx-[auto] my-[auto] h-full'>
         <header className='flex flex-col w-fit h-[80vh] justify-center'>
-          <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-red-500 to-yellow-400 text-7xl font-extrabold  leading-[76px]'>Leonardo Torrico</h1> <p className='text-pink-100 text-[24px] leading-8'>Desarrollador de Software</p>
+          <h1 className='flex flex-wrap font-extrabold  leading-[76px]'>
+            <span className='bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 via-red-400 to-yellow-600 text-7xl'>Leonardo</span>
+            <span className='bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 via-red-400 to-indigo-600 text-7xl'>Torrico</span>
+          </h1>
+          <p className='text-pink-100 text-[24px] leading-8'>Desarrollador de Software</p>
         </header>
         <picture className='self-center'>
           <img className='h-[600px] object-contain' src='persona_sentada.png' alt='Retrato de Leonardo' />
         </picture>
+        <div id='circle' />
+        <div id='circle_' />
       </div>
     </section>
   )
