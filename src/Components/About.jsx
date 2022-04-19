9fc1a764485bdb2c1a2030fae5d7eea71b936846
leoncii <leonardotobe94@gmail.com>
2022-04-19 05:11:01 -0400

    Hero

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ export function About () {
         Sobre mí...
       </Title5>
       <Description only>
-        Soy un desarrollador de aplicaciones web y escritorio de confianza, en este momento estoy de freelancer trabajando con Next.js y <strong>JavaScript</strong>. Me gusta hacer "Penetration Tester" en entornos controlados como en <a href='https://www.hackthebox.com/' target='_blank' rel='noreferrer'>Hack The Box</a>.
+        Leonardo es un Desarrollador de Aplicaciones Web y Escritorio de confianza, en este momento esta de freelancer trabajando con Next.js y JavaScript. Le gusta mucho aprender por eso hace "Penetration Tester" en entornos controlados como en <a href='https://www.hackthebox.com/' target='_blank' rel='noreferrer'>Hack The Box</a>.
       </Description>
     </>
   )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,9 @@
+import { Svg } from './Svg/CV'
+
 export function Hero () {
   return (
     <section id='hero' className='bg-[#101010] h-screen w-full pb-8 pl-4 md:pl-9 pr-4 md:pr-9 mb-32 md:mb-0'>
-      <div className='md:flex items-center justify-between h-screen md:flex-row max-w-[1200px] mx-[auto] my-[auto] flex flex-col'>
+      <div className='md:flex items-center justify-between h-full md:flex-row max-w-[1200px] mx-[auto] my-[auto] flex flex-col'>
         <header className='flex pt-8 mx-[auto] md:ml-[0] flex-col w-fit md:h-[80vh] justify-center  max-w-2xl'>
           <h1 id='name' className='flex flex-wrap justify-center font-extrabold  leading-[76px] md:flex-col lg:flex-row'>
             <span id='leonardo' className='bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 via-red-400 to-yellow-600 text-6xl sm:text-7xl'>Leonardo</span>
@@ -9,16 +11,16 @@ export function Hero () {
           </h1>
           <p id='namep' className='text-pink-100 text-lg sm:text-[24px] leading-8 lg:pb-12'>Desarrollador de Software</p>
           <div id='buttons1'>
-            <button id='button'>Hover me!</button>
-            <button id='button'>Hover me!</button>
+            <button id='button' title='Descargar CV' download ><Svg /></button>
+            <button id='button'><Svg /></button>
           </div>
         </header>
         <picture className='self-center'>
           <img id='photo' className='mb-8 object-cover mx-[auto] rotate-[25deg] rounded-[50%]' src='photo1.webp' alt='Retrato de Leonardo' />
         </picture>
         <div id='buttons' className='mx-[auto] w-full flex flex-col justify-between text-center'>
-          <button id='button' className='w-full md:mr-4 mb-4'>#</button>
-          <button id='button' className='w-full mb-4'>#</button>
+          <button id='button' title='Descargar CV'> className='w-full md:mr-4 mb-4'><Svg /></button>
+          <button id='button' className='w-full mb-4'><Svg /></button>
         </div>
       </div>
       <div id='circle' />
