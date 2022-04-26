38e7ab57ae88366461f8f5f8c8671c68808f6811
leonardo <leonardotobe94@gmail.com>
2022-04-26 14:18:28 -0400

    Typewriter node_modules...

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,33 +1,60 @@
 import { useEffect, useRef, useState } from 'react'
-/* import Typewriter from '../../node_modules/typewriter-effect/dist/core' */
-import Typewriter from 'typewriter-effect/dist/core'
+import Typewriter from '../../node_modules/typewriter-effect/dist/core'
+/* import Typewriter from 'typewriter-effect/dist/core' */
 
 export const Editor = () => {
   const editor = useRef()
+  const [isVisible, setIsVisible] = useState(false)
 
-  const typewriter = new Typewriter(editor?.current, {
-    loop: false,
-    delay: 75,
-    wrapperClassName: 'typewriter-wrapper-editor'
-  })
+  const options = {
+    rootMargin: '0px',
+    threshold: 0.2
+  }
 
-  typewriter
-    .pauseFor(1500)
-    .typeString('<strong>return</strong>')
-    .typeString(' <Layout')
-    .typeString('>')
-    .pauseFor(350)
-    .typeString('<div></div>')
-    .typeString('<')
-    .typeString('<span>ListOfItems </span>')
-    .typeString('data={<span style="color: blue;">data</span>} /')
-    .typeString('>')
-    .typeString('<div></div>')
-    .pauseFor(850)
-    .typeString('</ Layout')
-    .typeString('>')
-    .stop()
-    .start()
+  const handleObserver = (entries, observer) => {
+    const [entry] = entries
+
+    if (entry.isIntersecting) {
+      setIsVisible(true)
+      isVisible && printer()
+    }
+  }
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(handleObserver, options)
+    if (editor.current) observer?.observe(editor.current)
+
+    return () => {
+      if (editor.current) observer?.unobserve(editor.current)
+    }
+  }, [editor.current, options])
+
+  function printer () {
+    if (editor?.current) {
+      const typewriter = new Typewriter(editor?.current, {
+        loop: false,
+        delay: 75,
+        wrapperClassName: 'typewriter-wrapper-editor'
+      })
+      return typewriter
+        .pauseFor(1500)
+        .typeString('<strong>return</strong>')
+        .typeString(' <Layout')
+        .typeString('>')
+        .pauseFor(350)
+        .typeString('<div></div>')
+        .typeString('<')
+        .typeString('<span>ListOfItems </span>')
+        .typeString('data={<span style="color: blue;">data</span>} /')
+        .typeString('>')
+        .typeString('<div></div>')
+        .pauseFor(850)
+        .typeString('</ Layout')
+        .typeString('>')
+        .stop()
+        .start()
+    }
+  }
 
   return (
     <div className='mt-8 max-w-[540px] h-96 max-h-[608px] mx-4 shadow-sm md:mx-[auto]'>
